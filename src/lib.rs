77d28fb84bc43e6b6038b41612068b788c50c093
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-04 21:54:11 +0300

    feat: updated main cmd with fulll prove & verify pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,53 @@ pub mod examples;
 pub mod test_utils;
 pub mod vm;
 pub mod zkvm;
+
+use crate::{
+    air::TraceTable,
+    backend::FriOptions,
+    dsl::compile,
+    test_utils::{pick_coset_shift, pick_domain},
+    vm::{VmState, rows_to_trace_table, run_rows},
+    zkvm::{ZkvmProof, ZkvmPublicParameters},
+};
+use ark_bn254::Fr;
+use ark_ff::Zero;
+
+pub struct ProveResult {
+    pub proof: ZkvmProof<Fr>,
+    pub prove_duration: std::time::Duration,
+}
+
+pub fn setup_public_params(trace_size: usize) -> ZkvmPublicParameters<Fr> {
+    let blowup = 16usize;
+    let lde_size = trace_size * blowup;
+
+    let shift = pick_coset_shift(lde_size);
+    let trace_domain = pick_domain(trace_size);
+    let lde_domain = pick_domain(lde_size);
+
+    ZkvmPublicParameters {
+        trace_domain,
+        lde_domain,
+        shift,
+        fri_options: FriOptions {
+            max_degree: trace_size * 4,
+            max_remainder_degree: 1,
+            query_number: 64,
+            shift,
+        },
+    }
+}
+
+pub fn run_program_to_trace(source: &str, trace_len: usize, max_steps: usize) -> TraceTable<Fr> {
+    let program = compile(source).expect("program should compile");
+    let initial_state = VmState {
+        pc: 0,
+        regs: [Fr::zero(); 4],
+        halted: false,
+    };
+
+    let rows = run_rows(initial_state, &program, trace_len, max_steps).expect("program should run");
+
+    rows_to_trace_table(&rows)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::path::PathBuf;
-
+use ark_bn254::Fr;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use clap::{Parser, Subcommand};
+use std::{fs, path::PathBuf};
+use toy_zkvm::{
+    backend::Transcript,
+    run_program_to_trace, setup_public_params,
+    vm::VmAir,
+    zkvm::{ZkvmProof, ZkvmPublicParameters, prove, verify},
+};
 
 #[derive(Debug, Parser)]
 #[command(name = "toy-zkvm")]
@@ -16,29 +23,73 @@ enum Command {
         #[arg(long)]
         src: PathBuf,
 
-        #[arg(long, default_value = "./proof.json")]
+        #[arg(long, default_value = "./proof.bin")]
         proof: PathBuf,
+
+        #[arg(long, default_value = "public_params.bin")]
+        params: PathBuf,
     },
     Verify {
         #[arg(long)]
-        src: PathBuf,
-
-        #[arg(long)]
         proof: PathBuf,
+
+        #[arg(long, default_value = "public_params.bin")]
+        params: PathBuf,
     },
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::Prove { src, proof } => {
+        Command::Prove {
+            src,
+            proof: proof_path,
+            params: params_path,
+        } => {
             println!("Proving source:{}", src.display());
-            println!("Writing proof to: {}", proof.display());
+            println!("Writing proof to: {}", proof_path.display());
+
+            let src_str = fs::read_to_string(src)?;
+
+            let trace = run_program_to_trace(&src_str, 256, 256);
+            let air = VmAir::<Fr>::default();
+
+            let public_params = setup_public_params(trace.n());
+
+            let mut tx = Transcript::new(b"transcript", b"toy_zkvm_cli");
+
+            let proof = prove(&trace, &air, &mut tx, &public_params)?;
+
+            let mut proof_bytes = Vec::new();
+            proof.serialize_compressed(&mut proof_bytes)?;
+            fs::write(proof_path, proof_bytes)?;
+
+            let mut params_bytes = Vec::new();
+            public_params.serialize_compressed(&mut params_bytes)?;
+            fs::write(params_path, params_bytes)?;
+            Ok(())
         }
-        Command::Verify { src, proof } => {
-            println!("Verifying source:{}", src.display());
-            println!("Using proof: {}", proof.display());
+        Command::Verify {
+            proof: proof_path,
+            params: params_path,
+        } => {
+            println!("Using proof: {}", proof_path.display());
+
+            let proof_bytes = fs::read(&proof_path)?;
+            let proof = ZkvmProof::<Fr>::deserialize_compressed(&proof_bytes[..])?;
+
+            let params_bytes = fs::read(&params_path)?;
+            let public_params =
+                ZkvmPublicParameters::<Fr>::deserialize_compressed(&params_bytes[..])?;
+
+            let air = VmAir::<Fr>::default();
+            let mut tx = Transcript::new(b"transcript", b"toy_zkvm_cli");
+
+            verify(&proof, &air, &mut tx, &public_params)?;
+
+            println!("Proof verified successfully");
+            Ok(())
         }
     }
 }
